0a45108d9c51ff3cb83f7f4f9b5e974055485d34
alex sanchez <alexsanchez.az2005@gmail.com>
2025-04-18 13:07:22 -0500

    cambio de logo y de varias cosas

--- view/inicio/principal.php
+++ view/inicio/principal.php
@@ -1,52 +1,42 @@
 <main class="app-content">
       <div class="app-title">
         <div>
-          <h1><i class="bi bi-speedometer"></i> Dashboard</h1>
-          <p>A free and open source Bootstrap 5 admin template</p>
+          <h1><i class="bi "></i> Bienvenido</h1>
+          <p></p>
         </div>
-        <ul class="app-breadcrumb breadcrumb">
-          <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
-          <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-        </ul>
+
       </div>
       <div class="row">
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small primary coloured-icon"><i class="icon bi bi-people fs-1"></i>
+          <div class="widget-small primary coloured-icon">
             <div class="info">
-              <h4>Users</h4>
+              <h4>Total de Alumnos</h4>
               <p><b>5</b></p>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small info coloured-icon"><i class="icon bi bi-heart fs-1"></i>
+          <div class="widget-small info coloured-icon">
             <div class="info">
-              <h4>Likes</h4>
+              <h4>Entrenadores</h4>
               <p><b>25</b></p>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
-          <div class="widget-small warning coloured-icon"><i class="icon bi bi-folder2 fs-1"></i>
+          <div class="widget-small warning coloured-icon">
             <div class="info">
-              <h4>Uploades</h4>
+              <h4>Pagos Pendientes</h4>
               <p><b>10</b></p>
             </div>
           </div>
         </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="widget-small danger coloured-icon"><i class="icon bi bi-star fs-1"></i>
-            <div class="info">
-              <h4>Stars</h4>
-              <p><b>500</b></p>
-            </div>
-          </div>
-        </div>
+
       </div>
       <div class="row">
         <div class="col-md-6">
           <div class="tile">
-            <h3 class="tile-title">Weekly Sales - Last week</h3>
+            <h3 class="tile-title">Pronto estara el calendario</h3>
             <div class="ratio ratio-16x9">
               <div id="salesChart"></div>
             </div>
@@ -54,7 +44,7 @@
         </div>
         <div class="col-md-6">
           <div class="tile">
-            <h3 class="tile-title">Support Requests</h3>
+            <h3 class="tile-title">Asistencia Semanal</h3>
             <div class="ratio ratio-16x9">
               <div id="supportRequestChart"></div>
             </div>
