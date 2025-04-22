6bbf245b82a62a386d06cd71f4052b70bb5b0252
alex sanchez <alexsanchez.az2005@gmail.com>
2025-04-22 15:20:00 -0500

    importacion de las clases
    se importo las clases

---
+++ view/instructor/principal.php
@@ -0,0 +1,72 @@
+<main class="app-content">
+  <div class="app-title d-flex justify-content-between align-items-center">
+    <div>
+      <h1><i class="bi bi-speedometer"></i> BIENVENIDO</h1>
+    </div>
+    <ul class="app-breadcrumb breadcrumb">
+      <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
+      <li class="breadcrumb-item active">Dashboard</li>
+    </ul>
+  </div>
+  <div class="row mb-4">
+    <div class="col-md-6">
+      <div class="tile">
+        <h3 class="tile-title">Calendario</h3>
+        <input type="date" class="form-control">
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="tile">
+        <h3 class="tile-title">Lista de alumnos</h3>
+        <div class="list-group">
+          <!-- ITEM -->
+          <div class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+              <strong>Estudiante</strong><br>
+              <small>Horario (No establecido)</small>
+            </div>
+            <div>
+              <label class="me-2">
+                <input type="checkbox" class="form-check-input me-1"> Asistió
+              </label>
+              <label>
+                <input type="checkbox" class="form-check-input me-1"> No asistió
+              </label>
+            </div>
+          </div>
+
+          <!-- ITEM -->
+          <div class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+              <strong>Estudiante</strong><br>
+              <small>Horario (No establecido)</small>
+            </div>
+            <div>
+              <label class="me-2">
+                <input type="checkbox" class="form-check-input me-1"> Asistió
+              </label>
+              <label>
+                <input type="checkbox" class="form-check-input me-1"> No asistió
+              </label>
+            </div>
+          </div>
+
+          <!-- ITEM -->
+          <div class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+              <strong>Estudiante</strong><br>
+              <small>Horario (No establecido)</small>
+            </div>
+            <div>
+              <label class="me-2">
+                <input type="checkbox" class="form-check-input me-1"> Asistió
+              </label>
+              <label>
+                <input type="checkbox" class="form-check-input me-1"> No asistió
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
